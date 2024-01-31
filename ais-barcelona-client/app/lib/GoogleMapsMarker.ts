843c5b523ea4c75d31fec4bc240b0be8ba3ca599
Xavier Graells <fxgraells@outlook.com>
2024-01-31 15:41:11 +0100

    feat: add img for each ship_type

--- ais-barcelona-client/app/lib/GoogleMapsMarker.ts
+++ ais-barcelona-client/app/lib/GoogleMapsMarker.ts
@@ -5,7 +5,6 @@ const GoogleMapsMarker = ({
   map: google.maps.Map;
   sentences: any[];
 }) => {
-  console.log("sentences", sentences);
   if (!Array.isArray(sentences)) {
     console.error("sentences is not an array");
     return;
@@ -26,12 +25,10 @@ const GoogleMapsMarker = ({
       });
 
       sentences.forEach((sentence) => {
-        const tankerImg = document.createElement("img");
+        const shipImg = document.createElement("img");
         if (sentence.ship_type) {
-          tankerImg.src =
-            sentence.ship_type > 79 && sentence.ship_type < 90
-              ? "/tanker.png"
-              : "/ship.png";
+          const shipImgSrc = getShipImageUrl(sentence.ship_type);
+          shipImg.src = shipImgSrc;
         }
         if (sentence.lat && sentence.lon) {
           let collisionBehavior = google.maps.CollisionBehavior.REQUIRED;
@@ -39,7 +36,7 @@ const GoogleMapsMarker = ({
             map,
             position: { lat: sentence.lat, lng: sentence.lon },
             title: sentence.name,
-            content: tankerImg,
+            content: shipImg,
             collisionBehavior,
           });
 
@@ -74,3 +71,73 @@ const GoogleMapsMarker = ({
 };
 
 export default GoogleMapsMarker;
+
+const getShipImageUrl = (shipType: number) => {
+  switch (shipType) {
+    case 30:
+      return "/fishing.png";
+    case 31:
+    case 32:
+      return "/towing.png";
+    case 33:
+      return "/dredging.png";
+    case 34:
+      return "/diving.png";
+    case 35:
+      return "/military.png";
+    case 36:
+      return "/sailing.png";
+    case 37:
+      return "/pleasure_craft.png";
+    case 50:
+      return "/pilot_vessel.png";
+    case 51:
+      return "/search_rescue.png";
+    case 52:
+      return "/tug.png";
+    case 53:
+      return "/port_tender.png";
+    case 54:
+      return "/anti_pollution.png";
+    case 55:
+      return "/law_enforcement.png";
+    case 58:
+      return "/medical_transport.png";
+    case 60:
+    case 61:
+    case 62:
+    case 63:
+    case 64:
+    case 65:
+    case 66:
+    case 67:
+    case 68:
+    case 69:
+      return "/passenger.png";
+    case 70:
+    case 71:
+    case 72:
+    case 73:
+    case 74:
+    case 75:
+    case 76:
+    case 77:
+    case 78:
+    case 79:
+      return "/cargo.png";
+    case 80:
+    case 81:
+    case 82:
+    case 83:
+    case 84:
+    case 85:
+    case 86:
+    case 87:
+    case 88:
+    case 89:
+      return "/tanker.png";
+    default:
+      return "/ship.png";
+  }
+};
+
