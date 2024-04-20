c49d75a75b8be3966bcee65c94aaea12c277d9f1
Xavier Graells <fxgraells@outlook.com>
2024-04-20 22:49:07 +0200

    style: add theme switch and done styling

--- ais-barcelona-client/app/layout.tsx
+++ ais-barcelona-client/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { ThemeProviders } from './components/theme/theme-provider';
+import NavBar from './components/ui/NavBar';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,7 +19,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <ThemeProviders>
+          <NavBar />
+
+          {children}
+        </ThemeProviders>
+      </body>
     </html>
   );
 }
