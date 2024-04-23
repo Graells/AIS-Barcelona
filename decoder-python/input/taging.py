a439a7c8accebf5f440a6b2ad0ee1c08ba2ca904
Xavier Graells <fxgraells@outlook.com>
2024-04-23 13:21:52 +0200

    remove catched large files

--- decoder-python/input/taging.py
+++
@@ -1,5 +0,0 @@
-prefix = "\\c:1707859704.033334*A\\"
-
-with open('freshAIS.txt', 'r') as original, open('tags.txt', 'w') as modified:
-    for line in original:
-        modified.write(prefix + line)
